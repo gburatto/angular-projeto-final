9be9c61fa0c923f171209f63c48192a7ad816a87
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2024-11-09 14:37:46 +0000

    Cria layout responsivo para a `HomeComponent`.

--- apps/front/src/app/components/home/home.component.ts
+++ apps/front/src/app/components/home/home.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  inject,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    MatGridListModule,
+    CommonModule,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+
+  private breakpointObserver = inject(BreakpointObserver);
+
+  public umaColuna$ = this.breakpointObserver.observe([
+    Breakpoints.XSmall,
+    Breakpoints.Small,
+  ]).pipe(map(state => state.matches));
+
+  public duasColunas$ = this.breakpointObserver.observe([
+    Breakpoints.Medium,
+  ]).pipe(map(state => state.matches));
+
+}
