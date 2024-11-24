a658b3375f511cb6f6ea8cbe8682364ef80a9321
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2024-11-24 12:08:58 +0000

    Implementa template do formulário de login.

--- libs/auth/src/lib/auth/auth.component.ts
+++ libs/auth/src/lib/auth/auth.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
 @Component({
   selector: 'lib-auth',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css',
 })
-export class AuthComponent {}
+export class AuthComponent {
+
+  public hide = true;
+
+}
