8dd02b7775ebe1d70009ae1360c072906435297f
Gustavo Buratto <gustavo_buratto@hotmail.com>
2024-12-02 23:42:22 +0000

    Cria o `PratoService.getAll()`.

--- apps/front/src/app/services/prato.service.ts
+++ apps/front/src/app/services/prato.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { API_BASE } from '@nx-monorepo/auth';
+import { IPrato } from '@nx-monorepo/comum';
+import { Observable, shareReplay } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PratoService {
 
-  constructor() { }
+  private httpClient = inject(HttpClient);
+  private apiBase = inject(API_BASE);
+
+  public getAll(): Observable<IPrato[]> {
+    return this.httpClient.get<IPrato[]>(
+      `${this.apiBase}/prato`,
+    ).pipe(
+      shareReplay(),
+    );
+  }
+
 }
