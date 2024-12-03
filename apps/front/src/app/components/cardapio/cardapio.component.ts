01e479e80f7419d30fae6cbfe8d204a267f5134f
Gustavo Buratto <gustavo_buratto@hotmail.com>
2024-12-03 00:39:09 +0000

    Implementa o `VegetarianoPipe`.

--- apps/front/src/app/components/cardapio/cardapio.component.ts
+++ apps/front/src/app/components/cardapio/cardapio.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { PratoService } from '../../services/prato.service';
+import { VegetarianoPipe } from '../../pipes/vegetariano/vegetariano.pipe';
 
 @Component({
   selector: 'app-cardapio',
@@ -9,6 +10,7 @@ import { PratoService } from '../../services/prato.service';
   imports: [
     CommonModule,
     MatCardModule,
+    VegetarianoPipe,
   ],
   templateUrl: './cardapio.component.html',
   styleUrl: './cardapio.component.css',
@@ -17,5 +19,5 @@ export class CardapioComponent {
 
   private pratoService = inject(PratoService);
   public prato$ = this.pratoService.getAll();
-  
+
 }
--- apps/front/src/app/pipes/vegetariano/vegetariano.pipe.ts
+++ apps/front/src/app/pipes/vegetariano/vegetariano.pipe.ts
@@ -5,7 +5,18 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true,
 })
 export class VegetarianoPipe implements PipeTransform {
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+
+  /**
+   * Retorna a string `'Vegetariano'` ou `'Não Vegetariano'` e a classe correspondente, de acordo com o valor booleano.
+   *
+   * @param bool Valor booleano a ser verificado.
+   */
+  public transform(
+    bool: boolean,
+  ): { texto: string, classe: string } {
+    return bool
+    ? { texto: 'Vegetariano', classe: 'vegetariano'}
+    : { texto: "Não Vegetariano", classe: "nao-vegetariano"};
   }
+  
 }
