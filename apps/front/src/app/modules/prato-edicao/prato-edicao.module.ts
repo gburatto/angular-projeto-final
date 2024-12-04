e486409c51daa7653c05b47a10c57891a3e3c12e
Gustavo Buratto <gustavo_buratto@hotmail.com>
2024-12-04 00:53:05 +0000

    Gera `PratoEdicaoModule`.

---
+++ apps/front/src/app/modules/prato-edicao/prato-edicao.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PratoEdicaoRoutingModule } from './prato-edicao-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PratoEdicaoRoutingModule
+  ]
+})
+export class PratoEdicaoModule { }
